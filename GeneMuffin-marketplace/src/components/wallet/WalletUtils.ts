d76ef229b8e74aecba4cd48b5a6337f055613ff5
JackyKit <jackykit@gmail.com>
2025-05-19 15:58:24 +0800

    Add Branch
    set Branch for cloudflare deployments

--- GeneMuffin-marketplace/src/components/wallet/WalletUtils.ts
+++
@@ -1,8 +0,0 @@
-
-export const shortenAddress = (address: string): string => {
-  return address.slice(0, 6) + "..." + address.slice(-4);
-};
-
-export const formatPrice = (price: number): string => {
-  return `${price} ETH`;
-};
